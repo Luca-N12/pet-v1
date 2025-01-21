a1a9ec9888f117fd972476ed33143a6c9cd3729f
Luca-N12 <samallenluca70@gmail.com>
2025-01-21 13:24:53 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,163 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        # . . . #
+        . . . . #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . # . . #
+        . . . . #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . # . #
+        . . . . #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . . # #
+        . . . . #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . # #
+        . . # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . . #
+        . . # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . # # .
+        `)
+})
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+	
+})
+for (let index = 0; index < 648752399543456; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        . . . . .
+        # # # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        . . . . .
+        # # # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        . . # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        . . # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+}
